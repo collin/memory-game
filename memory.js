b090531c4a70c91a9624319ebcd934db93ff3191
pawanbenjamin <pawanbenjamin@gmail.com>
2023-03-15 08:51:27 -0500

    added player name input form

--- memory.js
+++ memory.js
@@ -1,118 +1,128 @@
 // a card
-const cards = [];
+const cards = []
 // build up cards list
-for (let cardCount = 0; cardCount < 3; cardCount++) {
-  cards.push({ name: cardCount + 1 });
-  cards.push({ name: cardCount + 1 });
+for (let cardCount = 0; cardCount < 8; cardCount++) {
+  cards.push({ name: cardCount + 1 })
+  cards.push({ name: cardCount + 1 })
 }
 const gameState = {
   cards: cards,
   players: [
     {
       score: 0,
-      name: "Pawan",
+      name: "Player 1"
     },
     {
       score: 0,
-      name: "Collin",
-    },
+      name: "Player 2"
+    }
   ],
-  currentPlayerIndex: 0,
-};
+  currentPlayerIndex: 0
+}
 
-const gameboard = document.querySelector("#gameboard");
-const scoreboard = document.querySelector("#scoreboard");
+const form = document.querySelector("form")
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  const player1Name = e.target[0].value
+  const player2Name = e.target[1].value
+  gameState.players[0].name = player1Name
+  gameState.players[1].name = player2Name
+  renderScoreboard()
+})
+
+const gameboard = document.querySelector("#gameboard")
+const scoreboard = document.querySelector("#scoreboard")
 
 function renderGameboard() {
   for (const card of gameState.cards) {
-    const cardEl = document.createElement("div");
-    cardEl.classList.add("card");
-    cardEl.setAttribute("cardName", card.name);
-    cardEl.innerText = card.name;
-    gameboard.append(cardEl);
+    const cardEl = document.createElement("div")
+    cardEl.classList.add("card")
+    cardEl.setAttribute("cardName", card.name)
+    cardEl.innerText = card.name
+    gameboard.append(cardEl)
   }
 }
 
-renderGameboard();
+renderGameboard()
 
 function renderScoreboard() {
-  scoreboard.innerHTML = "";
+  scoreboard.innerHTML = ""
   for (let i = 0; i < gameState.players.length; i++) {
-    const player = gameState.players[i];
-    const playerEl = document.createElement("div");
-    const isPlayersTurn = i === gameState.currentPlayerIndex;
-    playerEl.classList.add("player");
+    const player = gameState.players[i]
+    const playerEl = document.createElement("div")
+    const isPlayersTurn = i === gameState.currentPlayerIndex
+    playerEl.classList.add("player")
     playerEl.innerHTML = `
      ${isPlayersTurn ? `<span>Current</span>` : ``}
       <label>Player: ${player.name}</label>
       <p>Score: ${player.score}</p>
-    `;
+    `
 
-    scoreboard.append(playerEl);
+    scoreboard.append(playerEl)
   }
 
   // if all cards are matched, player with highest score wins
   if (checkWin()) {
-    scoreboard.innerHTML += `<p>${getWinner()}</p>`;
+    scoreboard.innerHTML += `<p>${getWinner()}</p>`
   }
 }
 
-renderScoreboard();
+renderScoreboard()
 
 function checkWin() {
-  const removedCards = document.querySelectorAll(".removed");
-  return removedCards.length === gameState.cards.length;
+  const removedCards = document.querySelectorAll(".removed")
+  return removedCards.length === gameState.cards.length
 }
 function getWinner() {
-  const player1Score = gameState.players[0].score;
-  const player2Score = gameState.players[1].score;
+  const player1Score = gameState.players[0].score
+  const player2Score = gameState.players[1].score
 
   if (player1Score === player2Score) {
-    return "No winner, tie game!";
+    return "No winner, tie game!"
   } else if (player1Score > player2Score) {
-    return `${gameState.players[0].name} won!`;
+    return `${gameState.players[0].name} won!`
   } else {
-    return `${gameState.players[1].name} won!`;
+    return `${gameState.players[1].name} won!`
   }
 }
 
 gameboard.addEventListener("click", (e) => {
   if (e.target.classList.contains("card")) {
-    e.target.classList.add("flipped");
+    e.target.classList.add("flipped")
     // use css to filter out removed
-    const flippedCards = document.querySelectorAll(".flipped:not(.removed)");
+    const flippedCards = document.querySelectorAll(".flipped:not(.removed)")
 
-    console.log(flippedCards);
+    console.log(flippedCards)
     if (flippedCards.length < 2) {
-      return;
+      return
     } else {
       if (
         flippedCards[0].getAttribute("cardname") ===
         flippedCards[1].getAttribute("cardname")
       ) {
         setTimeout(() => {
-          gameState.players[gameState.currentPlayerIndex].score++;
+          gameState.players[gameState.currentPlayerIndex].score++
 
           // remove cards from board
-          flippedCards[0].classList.add("removed");
-          flippedCards[1].classList.add("removed");
+          flippedCards[0].classList.add("removed")
+          flippedCards[1].classList.add("removed")
 
-          renderScoreboard();
+          renderScoreboard()
 
           // switch current player, using modulo to wrap around to 0
-        }, 1000);
+        }, 1000)
       } else {
         setTimeout(() => {
           // switch current player, using modulo to wrap around to 0
           gameState.currentPlayerIndex =
-            (gameState.currentPlayerIndex + 1) % gameState.players.length;
+            (gameState.currentPlayerIndex + 1) % gameState.players.length
 
           // this happens if they don't match
-          flippedCards[0].classList.remove("flipped");
-          flippedCards[1].classList.remove("flipped");
-          renderScoreboard();
-        }, 1000);
+          flippedCards[0].classList.remove("flipped")
+          flippedCards[1].classList.remove("flipped")
+          renderScoreboard()
+        }, 1000)
       }
     }
   }
-});
+})
